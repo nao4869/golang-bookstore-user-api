f94cf80c99f4768b51facc220ffb63d2e0107ec4
naohito muraoka <muraokan@oregonstate.edu>
2020-06-21 13:43:20 +0900

    updated: controllers moved to original place

--- app/application.go
+++ app/application.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/nao4869/golang-bookstore-user-api/controllers"
 )
 
 var (
@@ -12,11 +11,11 @@ var (
 // StartApplication -
 func StartApplication() {
 	// url endpoints mappings
-	router.GET("/ping", ping.Ping)
-	router.GET("/users/:user_id", users.GetUser)
-	//router.GET("users/search", users.SearchUser)
+	router.GET("/ping", controllers.Ping)
+	router.GET("/users/:user_id", controllers.GetUser)
+	//router.GET("users/search", controllers.SearchUser)
 
-	router.POST("/users", users.CreateUser)
+	router.POST("/users", controllers.CreateUser)
 
 	router.Run()
 }
