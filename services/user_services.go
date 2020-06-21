abdf063bc2e3c45f02ab1ba3b4ba0ad682a67903
naohito muraoka <muraokan@oregonstate.edu>
2020-06-21 16:03:52 +0900

    Updated: unused import removed

--- services/user_services.go
+++ services/user_services.go
@@ -1,13 +1,11 @@
 package services
 
 import (
-	"net/http"
-
 	"github.com/nao4869/golang-bookstore-user-api/domain/users"
 	"github.com/nao4869/golang-bookstore-user-api/utils/errors"
 )
 
-// CreateUser - must not return both users and errors - only return one 
+// CreateUser - must not return both users and errors - only return one
 // if we return nil, nil - this will cause many error in users_controllers when handling nil error and returing nil result etc
 func CreateUser(user users.User) (*users.User, *errors.RestError) {
 	return &user, nil
