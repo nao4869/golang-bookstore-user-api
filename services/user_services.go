354d8b72c77490672ad3023c021dd63febb49d35
naohito muraoka <muraokan@oregonstate.edu>
2020-06-21 17:40:28 +0900

    Updated: users changed to user

--- services/user_services.go
+++ services/user_services.go
@@ -8,7 +8,7 @@ import (
 // CreateUser - must not return both users and errors - only return one
 // if we return nil, nil - this will cause many error in users_controllers when handling nil error and returing nil result etc
 func CreateUser(user users.User) (*users.User, *errors.RestError) {
-	if error := users.Validate(); error != nil {
+	if error := user.Validate(); error != nil {
 		return nil, error
 	}
 
