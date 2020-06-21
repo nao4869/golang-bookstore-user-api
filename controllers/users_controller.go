619f5d280a5ac69d761be0ee8494f220c2ab2315
naohito muraoka <muraokan@oregonstate.edu>
2020-06-21 14:14:51 +0900

    updated: CreateUser function updated to see incoming request

--- controllers/users_controller.go
+++ controllers/users_controller.go
@@ -1,13 +1,22 @@
 package controllers
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/nao4869/golang-bookstore-user-api/domain/users"
 )
 
 // CreateUser -
 func CreateUser(c *gin.Context) {
+	var user users.User
+	bytes, error := ioutil.ReadAll(c.Request.Body)
+	fmt.Println(user)
+	fmt.Println(error)
+	fmt.Println(bytes)
+
 	c.String(
 		http.StatusNotImplemented,
 		"Not implemented",
