310ab076421f42ae32ddd4129f38010d0579766e
naohito muraoka <muraokan@oregonstate.edu>
2020-06-22 11:05:17 +0900

    updated: user model removed from user.go

--- domain/users/user.go
+++ domain/users/user.go
@@ -1,25 +1,25 @@
-package users
+// package users
 
-import (
-	"strings"
+// import (
+// 	"strings"
 
-	"github.com/nao4869/golang-bookstore-user-api/utils/errors"
-)
+// 	"github.com/nao4869/golang-bookstore-user-api/utils/errors"
+// )
 
-// User - use `json:"id"` value to populate struct field ID
-type User struct {
-	ID          int64  `json:"id"`
-	FirstName   string `json:"first_name"`
-	LastName    string `json:"last_name"`
-	Email       string `json:"email"`
-	DateCreated string `json:"date_created"`
-}
+// // User - use `json:"id"` value to populate struct field ID
+// type User struct {
+// 	ID          int64  `json:"id"`
+// 	FirstName   string `json:"first_name"`
+// 	LastName    string `json:"last_name"`
+// 	Email       string `json:"email"`
+// 	DateCreated string `json:"date_created"`
+// }
 
-// Validate - assigning Validate method to user struct
-func (user *User) Validate() *errors.RestError {
-	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
-	if user.Email == "" {
-		return errors.NewBadRequestError("invalid email address")
-	}
-	return nil
-}
+// // Validate - assigning Validate method to user struct
+// func (user *User) Validate() *errors.RestError {
+// 	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+// 	if user.Email == "" {
+// 		return errors.NewBadRequestError("invalid email address")
+// 	}
+// 	return nil
+// }
