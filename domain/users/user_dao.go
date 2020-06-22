aed0b6633111ab57b2dd7ab56b39e303856b7bbd
naohito muraoka <muraokan@oregonstate.edu>
2020-06-22 11:03:28 +0900

    updated: user model moved struct moved to dao file

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -11,6 +11,24 @@ import (
 	"github.com/nao4869/golang-bookstore-user-api/utils/errors"
 )
 
+// User - use `json:"id"` value to populate struct field ID
+type User struct {
+	ID          int64  `json:"id"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email"`
+	DateCreated string `json:"date_created"`
+}
+
+// Validate - assigning Validate method to user struct
+func (user *User) Validate() *errors.RestError {
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+	if user.Email == "" {
+		return errors.NewBadRequestError("invalid email address")
+	}
+	return nil
+}
+
 // mock user DB
 var (
 	usersDB = make(map[int64]*User)
