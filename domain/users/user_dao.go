b5b867d6b0542c9705a75a0768d41154f3f1b3b7
naohito muraoka <muraokan@oregonstate.edu>
2020-06-23 10:10:08 +0900

    Added: users_dto.go created

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,31 +2,12 @@ package users
 
 import (
 	"fmt"
-	"strings"
 
 	// just for testing
 	_ "github.com/go-sql-driver/mysql"
 	users_db "github.com/nao4869/golang-bookstore-user-api/datasources/mysql/users_db"
 	"github.com/nao4869/golang-bookstore-user-api/utils/errors"
 )
-
-// User - use `json:"id"` value to populate struct field ID
-type User struct {
-	ID          int64  `json:"id"`
-	FirstName   string `json:"first_name"`
-	LastName    string `json:"last_name"`
-	Email       string `json:"email"`
-	DateCreated string `json:"date_created"`
-}
-
-// Validate - assigning Validate method to user struct
-func (user *User) Validate() *errors.RestError {
-	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
-	if user.Email == "" {
-		return errors.NewBadRequestError("invalid email address")
-	}
-	return nil
-}
 
 // mock user DB
 var (
