631c47d42dc23500353ed648eed5b52f4de183ac
naohito muraoka <muraokan@oregonstate.edu>
2020-06-24 22:28:30 +0900

    Added: users data transfer object refactored as new file

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,40 +2,11 @@ package users
 
 import (
 	"errors"
-	"strings"
 
 	"github.com/federicoleon/bookstore_utils-go/logger"
 	"github.com/federicoleon/bookstore_utils-go/rest_errors"
 	"github.com/nao4869/golang-bookstore-user-api/datasources/mysql/users_db"
 )
-
-// User - use `json:"id"` value to populate struct field ID
-type User struct {
-	ID          int64  `json:"id"`
-	FirstName   string `json:"first_name"`
-	LastName    string `json:"last_name"`
-	Email       string `json:"email"`
-	DateCreated string `json:"date_created"`
-	Status      string `json:"status"`
-	Password    string `json:"password"`
-}
-
-// Validate - assigning Validate method to user struct
-func (user *User) Validate() rest_errors.RestErr {
-	user.FirstName = strings.TrimSpace(user.FirstName)
-	user.LastName = strings.TrimSpace(user.LastName)
-
-	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
-	if user.Email == "" {
-		return rest_errors.NewBadRequestError("invalid email address")
-	}
-
-	user.Password = strings.TrimSpace(user.Password)
-	if user.Password == "" {
-		return rest_errors.NewBadRequestError("invalid password")
-	}
-	return nil
-}
 
 const (
 	errorNoRows     = "no rows in result set"
