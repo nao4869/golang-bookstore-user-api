e09193195186670ed0c88fabceafff3cc1268bf4
naohito muraoka <muraokan@oregonstate.edu>
2020-06-24 21:26:57 +0900

    Added: rest error custom struct & user domains functions return value changed to rest error class

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -1,14 +1,13 @@
 package users
 
 import (
-	"fmt"
 	"strings"
 
 	// just for testing
 
 	"github.com/federicoleon/bookstore_users-api/utils/mysql_utils"
+	"github.com/federicoleon/bookstore_utils-go/rest_errors"
 	"github.com/nao4869/golang-bookstore-user-api/datasources/mysql/users_db"
-	"github.com/nao4869/golang-bookstore-user-api/utils/errors"
 )
 
 // User - use `json:"id"` value to populate struct field ID
@@ -18,13 +17,23 @@ type User struct {
 	LastName    string `json:"last_name"`
 	Email       string `json:"email"`
 	DateCreated string `json:"date_created"`
+	Status      string `json:"status"`
+	Password    string `json:"password"`
 }
 
 // Validate - assigning Validate method to user struct
-func (user *User) Validate() *errors.RestError {
+func (user *User) Validate() rest_errors.RestErr {
+	user.FirstName = strings.TrimSpace(user.FirstName)
+	user.LastName = strings.TrimSpace(user.LastName)
+
 	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
 	if user.Email == "" {
-		return errors.NewBadRequestError("invalid email address")
+		return rest_errors.NewBadRequestError("invalid email address")
+	}
+
+	user.Password = strings.TrimSpace(user.Password)
+	if user.Password == "" {
+		return rest_errors.NewBadRequestError("invalid password")
 	}
 	return nil
 }
@@ -36,12 +45,11 @@ const (
 )
 
 // Get - user pointer in order to working on actual value in the memory
-func (user *User) Get() *errors.RestError {
+func (user *User) Get() rest_errors.RestErr {
 	// connect to mysql DB
 	stmt, error := users_db.Client.Prepare(queryInsertUser)
 	if error != nil {
-		fmt.Println("error when trying to prepare save user statement")
-		return errors.NewInternalServerError(fmt.Sprintf("error for saving user", error.Error()))
+		mysql_utils.ParseError(error)
 	}
 	defer stmt.Close()
 
@@ -50,16 +58,13 @@ func (user *User) Get() *errors.RestError {
 	result := stmt.QueryRow(user.ID)
 
 	if getError := result.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); error != nil {
-		if strings.Contains(getError.Error(), errorNoRows) {
-			return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.ID))
-		}
-		return errors.NewInternalServerError(fmt.Sprintf("error when trying to retrieve user %d", user.ID))
+		return mysql_utils.ParseError(getError)
 	}
 	return nil
 }
 
 // Save - save the user to the database
-func (user *User) Save() *errors.RestError {
+func (user *User) Save() rest_errors.RestErr {
 	// insert new user to DB - creating statement connect to the DB so we must defer after communicating with it
 	stmt, error := users_db.Client.Prepare(queryInsertUser)
 	if error != nil {
@@ -68,14 +73,14 @@ func (user *User) Save() *errors.RestError {
 	defer stmt.Close()
 
 	// Exec return Result & Error
-	insertResult, saveError := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	insertResult, saveError := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated, user.Status, user.Password)
 	if saveError != nil {
-		mysql_utils.ParseError(saveError)
+		return mysql_utils.ParseError(saveError)
 	}
 
 	userID, err := insertResult.LastInsertId()
 	if err != nil {
-		mysql_utils.ParseError(err)
+		return mysql_utils.ParseError(err)
 	}
 	user.ID = userID
 	return nil
