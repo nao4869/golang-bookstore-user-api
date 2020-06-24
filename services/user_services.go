05dd4d385d366be358ea287a0ed606159d98dec7
naohito muraoka <muraokan@oregonstate.edu>
2020-06-24 22:05:56 +0900

    Updated: user services refactored as interface

--- services/user_services.go
+++ services/user_services.go
@@ -5,24 +5,39 @@ import (
 	"github.com/nao4869/golang-bookstore-user-api/domain/users"
 )
 
-// GetUser -
-func GetUser(userID int64) (*users.User, rest_errors.RestErr) {
-	result := &users.User{ID: userID}
-	if error := result.Get(); error != nil {
-		return nil, error
-	}
-	return result, nil
+var (
+	// UsersService -
+	UsersService usersServiceInterface = &usersService{}
+)
+
+type usersService struct{}
+
+type usersServiceInterface interface {
+	CreateUser(users.User) (*users.User, rest_errors.RestErr)
 }
+
+// GetUser -
+// func GetUser(userID int64) (*users.User, rest_errors.RestErr) {
+// 	result := &users.User{ID: userID}
+// 	if error := result.Get(); error != nil {
+// 		return nil, error
+// 	}
+// 	return result, nil
+// }
 
 // CreateUser - must not return both users and errors - only return one
 // if we return nil, nil - this will cause many error in users_controllers when handling nil error and returing nil result etc
-func CreateUser(user users.User) (*users.User, rest_errors.RestErr) {
-	if error := user.Validate(); error != nil {
-		return nil, error
+func (s *usersService) CreateUser(user users.User) (*users.User, rest_errors.RestErr) {
+	if err := user.Validate(); err != nil {
+		return nil, err
 	}
 
-	if error := user.Save(); error != nil {
-		return nil, error
+	// user.Status = users.StatusActive
+	// user.DateCreated = date_utils.GetNowDBFormat()
+	// user.Password = crypto_utils.GetMd5(user.Password)
+
+	if err := user.Save(); err != nil {
+		return nil, err
 	}
 	return &user, nil
 }
